681dacf5b042ce461d1abc4ed45c11dcc46f9aeb
Christian <chritiangabriel4@gmail.com>
2023-05-30 10:55:49 -0300

    add authorization

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const productsRoutes = require("./routes/products");
 const usersRoutes = require("./routes/users");
 const healthRoutes = require("./routes/health");
+const logger = require("./middleware/logger");
 
 const server = express();
 server.use(express.json());
+
+server.use(logger);
 
 server.use(productsRoutes.router);
 server.use(usersRoutes.router);
