3f7e79f98a72e12ef590fbadea6c00c2413d9f89
Christian <chritiangabriel4@gmail.com>
2023-05-31 20:21:57 -0300

    modificações

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const {saveProduct, getAllProducts, getProductById, updateProduct, deleteProduct, buyProductByUser} = require("../database/products");
 const auth = require("../middleware/auth");
+const z = require("zod");
 const router = express.Router();
+
+const ProductSchema = z.object({
+    name: z.string({
+        required_error: "Name must be required",
+        invalid_type_error: "Name must be a string",
+    }),
+    price: z.number().min(0).default(0),
+});
 
 router.get("/products", auth, async (req, res) => {
     const moreThan = req.query.more_than ? Number(req.query.more_than) : 0;
@@ -20,22 +29,24 @@ router.get("/products/:id", auth, async (req, res) => {
 }) 
 
 router.post("/products", auth, async (req, res) => {
-    const newProducts = {
-        name: req.body.name,
-        price: req.body.price
-    }
-    const savedProduct = await saveProduct(newProducts)
-    res.json({
-        product: savedProduct
+    try {
+        const newProduct = ProductSchema.parse(req.body);
+        const savedProduct = await saveProduct(newProduct)
+        res.json({
+            product: savedProduct
     })
+    } catch (error) {
+        if (err instanceof z.ZodError)
+        return res.status(422).json({
+            message: err.errors,
+        })
+        res.status(500).json({ message: "Server Error" });
+    }
 })
 
 router.put("/products/:id", auth, async (req, res) => {
     const id = Number(req.params.id)
-    const product = {
-        name: req.body.name,
-        price: req.body.price
-    }
+    const product = ProductSchema.parse(req.body);
     const updatedProduct = await updateProduct(id, product);
     res.json({
         product: updatedProduct
@@ -52,12 +63,12 @@ router.post("/products/buy", auth, async (req, res) => {
     const user = req.user;
     const productAndQuantity = req.body.products;
     for (let item of productAndQuantity) {
-      await buyProductByUser(user.userId, item.id, item.quantity);
-    }
+        await buyProductByUser(user.userId, item.id, item.quantity);    
+    }  
     res.status(201).json({
       success: true,
     });
-  });
+});
 
 module.exports = {
     router
--- routes/users.js
+++ routes/users.js
@@ -6,35 +6,42 @@ const jwt = require("jsonwebtoken");
 const auth = require("../middleware/auth");
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
+const userSchema = z.object({
+    name: z.string().min(3),
+    email: z.string().email(),
+    password: z.string().min(6),
+    age: z.number().optional(),
+  });
+
+  router.post("/register", async (req, res) => {
     try {
-        const isEmailAlreadyUsed = await findUserByEmail(req.body.email);
-        if(isEmailAlreadyUsed) 
-            return res.status(400).json({
-                massage: "Email already is being used."
-            })
-        const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-        const user = {
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        };
-        const savedUser = await saveUser(user);
-        delete savedUser.password;
-        res.status(201).json({
-            user: savedUser
+      const user = userSchema.parse(req.body);
+      const isEmailAlreadyUsed = await findUserByEmail(user.email);
+      if (isEmailAlreadyUsed)
+        return res.status(400).json({
+          message: "Email already is being used",
         });
-    } catch (error) {
-        res.status(500).json({
-            message: "Server error"
-        })
+      const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+      user.password = hashedPassword;
+      const savedUser = await saveUser(user);
+      delete savedUser.password;
+      res.status(201).json({
+        user: savedUser,
+      });
+    } catch (err) {
+      if (err instanceof z.ZodError)
+        return res.status(422).json({
+          message: err.errors,
+        });
+      res.status(500).json({
+        message: "Server error",
+      });
     }
-})
+  });
 
 router.post("/login", async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-
     const user = await findUserByEmail(email);
     if (!user) return res.status(401).send();
 
@@ -60,6 +67,13 @@ router.get("/profile", auth, async (req, res) => {
         user
     })
 })
+
+router.get("/history", auth, async (req, res) => {
+    const user = await findUserById(req.user.userId);
+    res.json({
+        user,
+    });
+});
 
 module.exports = {
     router
